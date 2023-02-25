855602cc952c845ad4dd906eed410cb53e030d0f
Tarek Kalaji <tarek.kalaji@etiya.com>
2023-02-26 01:45:41 +0300

    init 0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple Web
+A simple web app using Rust, serving html page 3 requests then shutting down!, using ThreadPool logic, this app for fun.
+
+This app no dependency required based on standard TCP in language.
+
+## To run
+```
+cargo run
+```
+Then browse to: http:127.0.0.1:8080
---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use std::io::Read;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
+use std::fs;
+use std::thread;
+use std::time::Duration;
+
+use simple_web::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    let pool = ThreadPool::new(4);
+    
+    // listener.incoming().take(2) allow service call twice
+    for stream in listener.incoming().take(3) { 
+        let stream = stream.unwrap();
+
+        pool.execute( || {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = 
+        if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK", "index.html")
+        } else if buffer.starts_with(sleep) {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    
+}
